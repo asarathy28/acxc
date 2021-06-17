c2683d9cccf41467e92a96c87a1cf77ad8325618
Ajay Sarathy <saraties2088@gmail.com>
2021-06-17 03:09:12 -0700

    good start

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home, name='home'),
     path('home/', views.home, name='home'),
+    path('short/', views.short, name='short'),
+    path('medium/', views.medium, name='medium'),
+    path('long/', views.long, name='long'),
+    path('truely-long/', views.truelyLong, name='truelyLong'),
+    path('destination/', views.destination, name='destination'),
 ]
--- main/views.py
+++ main/views.py
@@ -5,3 +5,23 @@ from django.shortcuts import render, redirect
 def home(request):
 
     return render(request, "main/home.html", {})
+
+def short(request):
+
+    return render(request, "main/short.html", {})
+
+def medium(request):
+
+    return render(request, "main/medium.html", {})
+
+def long(request):
+
+    return render(request, "main/long.html", {})
+
+def truelyLong(request):
+
+    return render(request, "main/truelyLong.html", {})
+
+def destination(request):
+
+    return render(request, "main/destination.html", {})
